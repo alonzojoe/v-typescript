ed5cf732ad41a267a8fa6a3c6feb202df815c25b
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-08-03 23:21:43 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Component from './components/Component.vue'
+import { onMounted, reactive } from 'vue';
+import { Person } from './types/Types'
+const person: Person = reactive({
+  name: "John Doe",
+  age: 23,
+  address: "123 Main Street America"
+})
+
+const handleChangeName = (newName: string) => {
+  person.name = newName
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Main Parent</h1>
+    <h3>Child Components</h3>
+    <Component :person="person" @changeName="handleChangeNames" />
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Component.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Person } from '../types/Types';
+
+// const props = defineProps({
+//     person: {
+//       type: Object as () => Person,
+//       required: true
+//     }
+// })
+
+const props = defineProps<{
+  person: Person
+}>()
+
+const emit = defineEmits(['changeName'])
+
+const newName = ref('')
+const changeName = () => {
+   const userInput = prompt("Enter New Name: ")
+   if (userInput !== null) {
+      newName.value = userInput ?? ''
+      emit('changeName', newName.value)
+   }
+
+}
+
+
+</script>
+
+<template>
+  <h1>Personal Information</h1>
+
+  <div class="card">
+      <p>Name: {{ person.name }}</p>
+      <p>Age: {{ person.age }}</p>
+      <p>Address: {{ person.address }}</p>
+      <button @click="changeName">Change Name</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
