b6f8de9e7b80b0a9e0cf9640405848c201b4b229
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-08-04 11:12:26 +0800

    Emitted

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,22 +1,5 @@
-// export interface Person {
-//     name: string;
-//     age: number;
-//     address: string;
-// }
-
-interface Person {
+export interface Person {
     name: string;
     age: number;
     address: string;
 }
-
-interface Fruits {
-    age: number
-}
-
-
-export default {
-    Person,
-    Fruits
-}
-
