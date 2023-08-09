93509e54aed034d613d9195ff1e3959f488e873e
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-08-09 08:27:10 +0800

    resolves conflict in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index'
-<<<<<<< HEAD
 import store from './store/index'
-=======
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
->>>>>>> 76ccd643eda0df1b05a3312886981601762dbb32
 
 const app = createApp(App)
 app.use(router)
