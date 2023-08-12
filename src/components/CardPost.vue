5b5af478f2c32e1397d34d5d0690a00dc22b88d6
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-08-12 23:48:26 +0800

    Modified CardPost, added comments section

--- src/components/CardPost.vue
+++ src/components/CardPost.vue
@@ -11,7 +11,7 @@ const emit = defineEmits()
 </script>
 
 <template>
-  <!-- <div class="card" style="width: 30rem;">
+  <div class="card" style="width: 30rem;">
     <div class="card-body">
       <div class="d-flex justify-content-start align-items-center mb-2">
         <img :src="post.owner.picture" class="cursor-pointer rounded-circle cst-img border border-primary" alt="...">
@@ -35,10 +35,41 @@ const emit = defineEmits()
         </div>
       </div>
     </div>
-  </div>  -->
+    <div class="accordion" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <!-- <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Comments
+          </button> -->
+          <a class="p-4 fs-6 text-comment" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><i class='bx bxs-message-rounded-dots'></i> Comments</a>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="d-flex justify-content-start align-items-center mb-2">
+              <img :src="post.owner.picture" class="cursor-pointer rounded-circle cst-img-comment border border-primary" alt="...">
+              <div class="d-flex align-items-center border rounded mx-2">
+                <div class="row p-0 m-0">
+                  <div class="col-12 p-0 m-0">
+                    <div class="d-flex align-items-center">
+                      <router-link :to="{name: 'profile', params: { id: post.owner.id } }" class="mx-2 fs-7 text-decoration-none">{{ post.owner.title }}. {{ post.owner.firstName }} {{ post.owner.lastName }}</router-link>
+                      <p class="mx-2 fs-6 text-secondary p-0 m-0">{{post.publishDate}}</p>
+                    </div>
+                  </div>
+                  <div class="col-12 p-0 m-0">
+                    <p class="mx-2 fs-6 text-secondary p-0 m-0">Nice Dog</p>
+                  </div>
+                </div>
+                 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
 
 
-  <div class="card" style="width: 30rem;">
+  <!-- <div class="card" style="width: 30rem;">
     <div class="card-body">
       <div class="d-flex justify-content-start align-items-center mb-2">
         <img src="../assets/placeholder.jpeg" class="cursor-pointer rounded-circle cst-img border border-secondary" alt="...">
@@ -63,7 +94,7 @@ const emit = defineEmits()
         </div>
       </div>
     </div>
-  </div> 
+  </div>  -->
 </template>
 
 <style scoped>
@@ -71,6 +102,15 @@ const emit = defineEmits()
 .cst-img{
   height: 3rem;
   width: auto;
+}
+
+.cst-img-comment{
+  height: 2rem;
+  width: auto;
+}
+
+.text-comment{
+  text-decoration: none;
 }
 
 </style>
