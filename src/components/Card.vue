80c3848798a27770806f1f4f9f07078996537ae9
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-08-21 23:08:39 +0800

    Re design UI of CardPerson and PersonPage

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -28,7 +28,7 @@ export default {
 </script>
 
 <template>
-  <div class="card" style="width: 20rem;">
+  <!-- <div class="card" style="width: 20rem;">
     <div class="card-body">
       <div class="d-flex justify-content-start align-items-center mb-2">
         <img :src="person.picture" class="cursor-pointer rounded-circle cst-img border border-primary" alt="...">
@@ -39,16 +39,66 @@ export default {
         
       </div>
     
-      <!-- <p class="card-text">{{ person }}</p> -->
+
       <div class="d-flex justify-content-end">
         <router-link :to="{name: 'person-posts', params: { id: person.id }}" class="btn btn-sm btn-primary">View Posts</router-link>
       </div>
     </div>
-  </div> 
+  </div>  -->
+  <div class="card">
+    <div class="card-body p-4 text-center border-bottom position-relative">
+      <router-link :to="{name: 'person-posts', params: { id: person.id }}" class="view-posts btn btn-primary">View Posts</router-link>
+      <img :src="person.picture" alt="" class="rounded-circle mb-3" width="80" height="80">
+      <h5 class="fw-semibold fs-4 mb-0"><a href="javascript:void(0);" @click="checkProfile(person.id)" class="text-decoration-none" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">{{ person.title }}. {{ person.firstName }} {{ person.lastName }}</a></h5>
+      <span class="text-dark fs-6">{{ person.id }}</span>
+      <ul class="px-2 py-2 bg-light list-unstyled d-flex align-items-center justify-content-center mb-0">
+        <li class="position-relative">
+          <a class="text-primary d-flex align-items-center justify-content-center p-2 mx-1 fs-5 cst-bg-primary rounded-circle fw-semibold" href="javascript:void(0)">
+            <i class='bx bxl-facebook text-white'></i>
+          </a>
+        </li>
+        <li class="position-relative">
+          <a class="text-danger d-flex align-items-center justify-content-center p-2 mx-1 fs-5 cst-bg-pink rounded-circle fw-semibold " href="javascript:void(0)">
+            <i class='bx bxl-instagram text-white'></i>
+          </a>
+        </li>
+        <li class="position-relative">
+          <a class="text-info d-flex align-items-center justify-content-center p-2 mx-1 fs-5 cst-bg-info rounded-circle fw-semibold " href="javascript:void(0)">
+            <i class='bx bxl-linkedin text-white'></i>
+          </a>
+        </li>
+        <li class="position-relative">
+          <a class="text-secondary d-flex align-items-center justify-content-center p-2 mx-1 bg-danger fs-5 rounded-circle fw-semibold " href="javascript:void(0)">
+            <i class='bx bxl-youtube text-white'></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 
+a {
+  text-decoration: none;
+}
+
+.view-posts {
+  position:absolute;
+  top: 0;
+  right: 0;
+}
+.cst-bg-primary {
+  background-color: #5972FB;
+}
+
+.cst-bg-pink {
+  background-color: #F40072;
+}
+
+.cst-bg-info {
+  background-color: #49B2FC;
+}
 .cst-img{
   height: 3rem;
   width: auto;
--- src/views/PersonsPage.vue
+++ src/views/PersonsPage.vue
@@ -33,45 +33,49 @@ const getIdFromChild = (data, callback) => {
 </script>
 
 <template>
-  <div class="row justify-content-center">
-    <ViewPerson v-show="offCanvasShow && profileId" :personId="profileId" />
-    <div class="col-12">
-      <div v-if="isLoadingPost" class="d-flex justify-content-center align-items-center">
-        <div class="spinner-border text-secondary" role="status">
-          <span class="visually-hidden">Loading...</span>
+  <div class="container">
+    <div class="row justify-content-center">
+      <ViewPerson v-show="offCanvasShow && profileId" :personId="profileId" />
+      <div class="col-12">
+        <div v-if="isLoadingPost" class="d-flex justify-content-center align-items-center">
+          <div class="spinner-border text-secondary" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-if="!isLoadingPost" v-for="p in persons" :key="p.id" class="col-sm-12 col-md-6 col-lg-4 mb-3">
-      <Card @transferId="getIdFromChild($event)" :person="p" />
-    </div>
-
-    <div class="col-sm-12 col-md-6 col-lg-3 mb-3">
-      <div class="card" aria-hidden="true">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg"
-          role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#868e96"></rect>
-        </svg>
-        <div class="card-body">
-          <h5 class="card-title placeholder-glow">
-            <span class="placeholder col-6"></span>
-          </h5>
-          <p class="card-text placeholder-glow">
-            <span class="placeholder col-7"></span>
-            <span class="placeholder col-4"></span>
-            <span class="placeholder col-4"></span>
-            <span class="placeholder col-6"></span>
-            <span class="placeholder col-8"></span>
-          </p>
-          <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
-        </div>
+      <div v-if="!isLoadingPost" v-for="p in persons" :key="p.id" class="col-sm-12 col-md-6 col-lg-4 mb-3">
+        <Card @transferId="getIdFromChild($event)" :person="p" />
       </div>
+  
+      <!-- <div class="col-sm-12 col-md-6 col-lg-3 mb-3">
+        <div class="card" aria-hidden="true">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg"
+            role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#868e96"></rect>
+          </svg>
+          <div class="card-body">
+            <h5 class="card-title placeholder-glow">
+              <span class="placeholder col-6"></span>
+            </h5>
+            <p class="card-text placeholder-glow">
+              <span class="placeholder col-7"></span>
+              <span class="placeholder col-4"></span>
+              <span class="placeholder col-4"></span>
+              <span class="placeholder col-6"></span>
+              <span class="placeholder col-8"></span>
+            </p>
+            <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+          </div>
+        </div>
+      </div> -->
     </div>
   </div>
+
 </template>
 
 <style scoped>
+
 .logo {
   height: 6em;
   padding: 1.5em;
