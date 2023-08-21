d636992ac431df1a2ad5da2c0ab8c09a9e7e1fe3
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2023-08-21 22:35:00 +0800

    Re create ui design of ViewProfile Component

--- src/components/ViewPerson.vue
+++ src/components/ViewPerson.vue
@@ -5,37 +5,49 @@
             <h5 class="offcanvas-title" id="offcanvasRightLabel">Profile</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body" v-if="!isLoadingProfile">
-            <pre>{{ profile }}</pre>
+        <div class="offcanvas-body" v-if="!isLoadingProfile && profile.location">
             <div class="d-flex-row align-items-start mb-7 pb-1 align-items-center justify-content-between">
                 <div class="d-flex align-items-center gap-2">
-
+                    <img :src="profile.picture" alt="user8" width="72" height="72" class="rounded-circle">
+                    <div>
+                        <h6 class="fw-semibold fs-5 mb-0">{{ profile.firstName }} {{ profile.lastName }}</h6>
+                        <p class="mb-0 text-secondary fs-6">{{ profile.email }}</p>
+                    </div>
+                </div>
+            
+            </div>
+            <div class="row mt-4">
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Title</p>
+                    <h6 class="fw-semibold fs-6 mb-2">"{{ profile.title }}."</h6>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Gender</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.gender }}</h6>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Birthdate</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.dateOfBirth }}</h6>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Phone number</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.phone }}</h6>
                 </div>
             </div>
-            <!-- <div class="row p-0 m-0">
-          <div class="col-12">
-            <div class="d-flex justify-content-center align-items-center mb-3">
-              <img :src="profile.picture" class="cursor-pointer rounded-circle cst-img" alt="...">
+            <div class="row">
+                <div class="col-sm-12 col-lg-12">
+                    <p class="mb-1 fs-6 text-secondary">Location</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.location.street }}, {{ profile.location.city }} {{ profile.location.state }}</h6>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Country</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.location.country }}</h6>
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p class="mb-1 fs-6 text-secondary">Timezone</p>
+                    <h6 class="fw-semibold fs-6 mb-2">{{ profile.location.timezone }}</h6>
+                </div>
             </div>
-          </div>
-          <div class="col-12">
-            <div class="d-flex justify-content-center align-items-center mb-3">
-              <h5 class="card-title">{{ profile.title }} {{ profile.firstName }} {{ profile.lastName }}</h5>
-            </div>
-          </div>
-          <div class="col-12">
-            <div class="d-flex justify-content-center align-items-center mb-3">
-              <h6 class="card-subtitle mb-2 text-body-secondary">{{ profile.email }} / {{ profile.phone }}</h6>
-            </div>
-          </div>
-        </div>
-   -->
-            <!-- <p class="card-text p-0 m-2">Gender: {{ profile.gender }}</p>
-        <p class="card-text p-0 m-2">Birthday: {{ profile.dateOfBirth }}</p>
-        <p class="card-text p-0 m-2">Address: {{ profile.location.street }} {{ profile.location.city }} {{
-          profile.location.state }}</p>
-        <p class="card-text p-0 m-2">Country: {{ profile.location.country }}</p>
-        <p class="card-text p-0 m-2">GMT Timezone: {{ profile.location.timezone }}</p> -->
         </div>
         <div v-if="isLoadingProfile" class="d-flex justify-content-center align-items-center mt-5">
             <div class="spinner-border text-secondary" role="status">
